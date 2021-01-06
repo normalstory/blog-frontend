85e54b92046d31d5c83eeacffd5fb02b795bf7b5
thinknormal <nornalstory@gmail.com>
2021-01-06 22:36:29 +0900

    05조회 - 1)포스트 읽기 UI 컴포넌트, page연동

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import PostViewer from '../components/post/PostViewer';
 
 const PostPage = () => {
-  return <div>포스트 페이지</div>;
+  return (
+    <>
+      <HeaderContainer />
+      <PostViewer />
+    </>
+  );
 };
 
 export default PostPage;
