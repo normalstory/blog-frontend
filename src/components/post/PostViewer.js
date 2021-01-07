2b5635b5f0a9316e97a3188f7fedffe5e92a527d
thinknormal <nornalstory@gmail.com>
2021-01-07 23:32:05 +0900

    05조회 - 2)포스트 api 및 page연동(수정_1)

--- src/components/post/PostViewer.js
+++ src/components/post/PostViewer.js
@@ -2,8 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
-import SubInfo from '../common/SubInfo';
-import Tags from '../common/Tags';
 
 const PostViewerBlock = styled(Responsive)`
   margin-top: 4rem;
@@ -18,37 +16,65 @@ const PostHead = styled.div`
     margin: 0;
   }
 `;
-
+const SubInfo = styled.div`
+  margin-top: 1rem;
+  color: ${palette.gray[6]};
+  //span 사이에 가운데점 문자 보여주기
+  span + span:before {
+    color: ${palette.gray[5]};
+    padding-left: 0.25rem;
+    padding-right: 0.25rem;
+    content: '\\B7'; //*가운데점 문자
+  }
+`;
+const Tags = styled.div`
+  margin-top: 0.5rem;
+  .tag {
+    display: inline-block;
+    color: ${palette.cyan[7]};
+  }
+  text-decoration: none;
+  margin-left: 0.5rem;
+  &:hover {
+    color: ${palette.gray[6]};
+  }
+`;
 const PostContent = styled.div`
   font-size: 1.3125rem;
   color: ${palette.gray[8]};
 `;
 
+//+ PostViewer 컴포넌트에 적용된 props를 사용
 const PostViewer = ({ post, error, loading }) => {
-  // 에러 발생 시
+  //에러 발생 시
   if (error) {
     if (error.response && error.response.status === 404) {
-      return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
+      return <PostViewerBlock> 존재하지 않는 포스트입니다. </PostViewerBlock>;
     }
-    return <PostViewerBlock>오류 발생!</PostViewerBlock>;
+    return <PostViewerBlock>오류 발생 !</PostViewerBlock>;
   }
-
-  // 로딩중이거나, 아직 포스트 데이터가 없을 시
+  //로딩 중이거나 아직 포스트 데이터가 없을 때
   if (loading || !post) {
     return null;
   }
 
   const { title, body, user, publishedDate, tags } = post;
+
   return (
     <PostViewerBlock>
       <PostHead>
         <h1>{title}</h1>
-        <SubInfo
-          username={user.username}
-          publishedDate={publishedDate}
-          hasMarginTop
-        />
-        <Tags tags={tags} />
+        <SubInfo>
+          <span>
+            <b>{user.username}</b>
+          </span>
+          <span>{new Date(publishedDate).toLocaleDateString()}</span>
+        </SubInfo>
+        <Tags>
+          {tags.map((tag) => (
+            <div className="tag">#{tag}</div>
+          ))}
+        </Tags>
       </PostHead>
       <PostContent dangerouslySetInnerHTML={{ __html: body }} />
     </PostViewerBlock>
